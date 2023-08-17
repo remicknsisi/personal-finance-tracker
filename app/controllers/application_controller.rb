d26a772aa7d392ce3e0e49c850b15fd453ddf396
Sienna <remicknsisi@gmail.com>
2023-08-17 13:23:07 -0400

    added sessions controller and before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  before_action :authorize
 
+  def authorize
+      return render json: { error: "Not Authorized" }, status: :unauthorized unless session.include? :user_id
+      @user = User.find_by(id: session[:user_id])
+  end
 end
