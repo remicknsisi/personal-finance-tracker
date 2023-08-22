b6d72d323f456a05aafe7ba8bd8d569cfd3c19dc
Sienna <remicknsisi@gmail.com>
2023-08-22 10:55:08 -0400

    added in sort funcitonality for transactions by date

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { useNavigate, Link } from "react-router-dom";
 import { UserContext } from "../context/UserProvider.js";
 import Budget from './Budget.js';
@@ -8,8 +8,15 @@ import Transaction from './Transaction.js';
 function Dashboard (){
     const { currentUser, logout } = useContext(UserContext)
     const [isChecked, setIsChecked] = useState(false)
+    const [transactions, setTransactions] = useState([])
 
     const navigate = useNavigate()
+
+    useEffect(() => {
+        fetch('/transactions')
+        .then(res => res.json())
+        .then(transactionData => setTransactions(transactionData))
+    }, [])
 
     function handleCheck(){
         setIsChecked(!isChecked)
@@ -24,12 +31,20 @@ function Dashboard (){
         })
     }
 
-    const budgetsToDisplay = currentUser ? isChecked ? "these are the budgets in order of date" : currentUser.budgets.map(b => {
+    const budgetsToDisplay = currentUser ? currentUser.budgets.map(b => {
         return <Budget budget={b} key={b.id}/>}): null
 
-    const transactionsToDisplay = currentUser ? currentUser.transactions.map(t => {
-        return <Transaction transaction={t} key={t.id}/>}): null
 
+    const allTransactions = transactions.map(t => {
+        return <Transaction transaction={t} key={t.id}/>})
+
+    const sortedTransactions = [...transactions].sort((t1, t2) => (t1.date > t2.date) ? 1 : (t1.date < t2.date) ? -1 : 0).map(t => {
+        return <Transaction transaction={t} key={t.id}/>})
+
+    const transactionsToDisplay = isChecked ? sortedTransactions : allTransactions
+
+
+    
     return (
         <div className="app">
             {currentUser ? <h1>Welcome {currentUser.name}!</h1> : null}
