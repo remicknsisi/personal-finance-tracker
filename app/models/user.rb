191fa8247c3018b33f6cbd7ed33af484e407073e
Sienna <remicknsisi@gmail.com>
2023-08-27 10:46:04 -0400

    added validations to new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
     has_many :budgets, dependent: :destroy
     has_many :transactions, dependent: :destroy
     has_many :tags, through: :transactions
+
+    validates :email, presence: true
+    validates :name, presence: true
 end
